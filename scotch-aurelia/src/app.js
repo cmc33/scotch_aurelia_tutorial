580ecbbf0f729e56f418fe3134c486ecb88c307a
cmc33 <cmcguigan33@gmail.com>
2016-06-12 22:02:06 -0400

    fooling around with routing and data binding

--- scotch-aurelia/src/app.js
+++ scotch-aurelia/src/app.js
@@ -1,3 +1,16 @@
 export class App {
+  // Properties
   message = 'Welcome to Aurelia!';
+  firstNum = 0;
+  secondNum = 0;
+
+  // Get method that returns the sum of the two integer properties
+  get sum () {
+    return parseInt(this.firstNum) + parseInt(this.secondNum);
+  }
+
+  submit () {
+    alert(`Sum of ${this.firstNum} and ${this.secondNum} is ${this.sum}`)
+  }
+
 }
